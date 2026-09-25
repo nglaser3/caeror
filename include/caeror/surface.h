7e3e0b81af834d9270aac08bb7a3be3e286655b5
Nathan Glaser <glaserna@oregonstate.edu>
2026-09-25 01:29:13 -0700

    added basic surface struct

--- include/caeror/surface.h
+++ include/caeror/surface.h
@@ -1,1 +1,42 @@
 #pragma once
+#include <camp/tuple.hpp>
+
+/**
+ * @brief Enum of allowed axes
+ */
+enum Axis {
+  X,
+  Y, 
+  Z
+};
+
+/**
+ * @brief Enum of all allowed surface types
+ */
+enum class SurfaceType {
+  Plane,
+  AxisAlignedPlane,
+  AxisAlignedCylinder,
+  Sphere,
+};
+
+/**
+ * @brief Container for surface type and underlying data to represent the surface
+ */
+template<SurfaceType Type, typename... DataTypes>
+struct Surface{
+  /// @brief Type of the surface
+  static constexpr SurfaceType surface_type_ = Type;
+
+  /// @brief Data used to define the surface
+  camp::tuple<DataTypes...> data_;
+};
+
+/// @brief Generic plane surface
+using Plane = Surface<SurfaceType::Plane, double, double, double, double>;
+/// @brief Axis aligned plane surface
+using AxisAlignedPlane = Surface<SurfaceType::AxisAlignedPlane, Axis, double>;
+/// @brief Axis aligned cylinder surface
+using AxisAlignedCylinder = Surface<SurfaceType::AxisAlignedCylinder, Axis, double, double>;
+/// @brief Sphere surface
+using Sphere = Surface<SurfaceType::Sphere, double, double, double, double>;

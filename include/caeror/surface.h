be528c67e6e6f7da79cffd8cee59b5b3b1625fe1
Nathan Glaser <glaserna@oregonstate.edu>
2026-09-25 01:38:25 -0700

    add enums for indexing into data

--- include/caeror/surface.h
+++ include/caeror/surface.h
@@ -34,9 +34,37 @@ struct Surface{
 
 /// @brief Generic plane surface
 using Plane = Surface<SurfaceType::Plane, double, double, double, double>;
+/// @brief Helper enum for accessing generic surface data
+enum class PlaneData {
+  A,
+  B, 
+  C,
+  D
+};
+
 /// @brief Axis aligned plane surface
 using AxisAlignedPlane = Surface<SurfaceType::AxisAlignedPlane, Axis, double>;
+/// @brief Helper enum for accessing axis aligned plane data
+enum class AxisAlignedPlaneData {
+  Axis,
+  Intercept
+};
+
 /// @brief Axis aligned cylinder surface
 using AxisAlignedCylinder = Surface<SurfaceType::AxisAlignedCylinder, Axis, double, double>;
+/// @brief Helper enum for accessing axis aligned cylinder data, centers are stored (x, y), (x, z), or (y, z)
+enum class AxisAlignedCylinderData {
+  Axis,
+  Center1,
+  Center2
+};
+
 /// @brief Sphere surface
 using Sphere = Surface<SurfaceType::Sphere, double, double, double, double>;
+/// @brief Helper enum for accessing sphere data
+enum class SphereData {
+  Radius,
+  XCenter,
+  YCenter,
+  ZCenter
+};
